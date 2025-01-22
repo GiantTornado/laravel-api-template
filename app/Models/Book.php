f2c7401143aedd09dd5385c34825dedffaa4b455
Mohamad Termanini <mohtermanini.job@gmail.com>
2025-01-22 23:50:54 +0400

    Clean Models Code

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,29 +2,54 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Book extends Model
-{
-    use HasFactory, SoftDeletes;
+class Book extends Model {
+    use HasFactory, Sluggable, SoftDeletes;
 
-    protected $fillable = ['title', 'slug', 'description', 'published_at', 'category_id'];
+    protected $table = 'books';
 
-    public function scopeNewlyCreated($query, $from_date)
-    {
-        $query->where("created_at", '>=', $from_date);
+    protected $fillable = ['title', 'slug', 'description', 'published_at', 'price', 'category_id'];
+
+    public function sluggable(): array {      // auto generate slug on model creating [composer require cviebrock/eloquent-sluggable]
+        return [
+            'slug' => [
+                'source' => 'title',         // name of column to generate [slug] from
+            ],
+        ];
     }
 
-    public function category()
-    {
+    protected function price(): Attribute {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => $value / 100,
+            set: fn(mixed $value, array $attributes) => $value * 100,
+        );
+    }
+
+    public function scopeNewlyCreated($query, $from_date) {
+        $query->where('created_at', '>=', $from_date);
+    }
+
+    /**
+     * Get the category that the book belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category() {
         // return $this->belongsTo(RelatedModel::class, 'foreign_key_in_current_model', 'primary_key_in_related_model')->chained_methods;
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
-    public function authors()
-    {
-        return $this->belongsToMany(Author::class)->withPivot('is_autographed')->orderBy("name", "desc");
+    /**
+     * Get the authors associated with the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function authors() {
+        return $this->belongsToMany(Author::class)->withPivot('is_autographed')->orderBy('name', 'desc');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,30 +9,36 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use HasFactory, Notifiable, HasApiTokens;
+class User extends Authenticatable {
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = ['email', 'password', 'role_id'];
 
     protected $hidden = ['password', 'remember_token'];
 
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
 
-    public function profile()
-    {
+    /**
+     * Get the profile associated with the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function profile() {
         // return $this->hasOne(RelatedModel::class, 'foreign_key_in_related_model', 'primary_key_in_current_model');
         return $this->hasOne(Profile::class, 'user_id', 'id');
     }
 
-    public function role()
-    {
+    /**
+     * Get the role that the user belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role() {
         // return $this->belongsTo(RelatedModel::class, 'foreign_key_in_current_model', 'primary_key_in_related_model')->chained_methods;
         return $this->belongsTo(Role::class, 'role_id', 'id');
     }
